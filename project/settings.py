b7bbbd3d4c9cac944d8f9b11e10fc55b2f5b257a
lokeshpatil01082002 <83952898+lokeshpatil01082002@users.noreply.github.com>
2023-02-22 00:35:50 +0530

    after adding final deploment files

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-c(f41sppn%dprr=s^y%t3jqo0po+3r%16)86^9d)4we#=hq^*7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
